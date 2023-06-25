64ad372ac7c42f42ec845670f4ab4fbf71661b32
JahanaSultan <cahana.aliyeva@gmail.com>
2023-06-25 13:06:43 +0400

    update

--- src/components/RentBookCard.tsx
+++ src/components/RentBookCard.tsx
@@ -3,6 +3,7 @@ import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import { FC } from "react";
+import Button from "@mui/material/Button";
 
 interface Props {
     id: number;
@@ -19,6 +20,9 @@ const RentBookCard: FC<Props> = ({id, name, image, author}) => {
         height: "200px",
         display: "flex",
         boxShadow: "none",
+        '&:hover .MuiCardContent-root': {
+          top: 0,
+        }
       }}
     >
       <CardMedia
@@ -29,7 +33,7 @@ const RentBookCard: FC<Props> = ({id, name, image, author}) => {
           width: "100%",
           height: "100%",
           objectFit: "contain",
-          transition: "all 0.5s ease",
+          transition: "0.1s",
         }}
       />
       <CardContent sx={{
@@ -37,15 +41,42 @@ const RentBookCard: FC<Props> = ({id, name, image, author}) => {
         backgroundColor: "#00001ddd",
         width: "100%",
         height: "100%",
-        top: 0,
+        top: '-200%',
         left: 0,
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "flex-end",
+        alignItems: "flex-start",
+        transition: "all 0.5s ease",
       }}>
-        <Typography gutterBottom variant="h5" component="div">
+        <Typography gutterBottom variant="h6" component="div"
+        sx={{
+            color: "#fff",
+            fontSize: "16px",
+            lineHeight: "1",
+            borderBottom: "2px solid var(--orange)",
+        }}>
             {name}
         </Typography>
-        <Typography variant="body2" color="text.secondary">
+        <Typography variant="body2" color="text.secondary"
+        sx={{
+          color: "var(--light-gray)",
+        }}
+        >
             {author}
         </Typography>
+        <Button
+        sx={{
+          color: "var(--orange)",
+          border: "1px solid var(--orange)",
+          width: "100%",
+          height: "25px",
+          padding: "0",
+          marginTop: "10px",
+        }}
+        >
+            Rent
+        </Button>
         </CardContent>
     </Card>
   );
